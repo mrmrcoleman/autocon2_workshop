bd861380b7e2c00975755971a1bc2b77f34c111e
Mark Coleman <mrmrcoleman@gmail.com>
2024-10-05 17:44:20 +0000

    Added install scripts for installing Docker, Clab and Slurpit

---
+++ 1_install_docker_and_clab.sh
@@ -0,0 +1,15 @@
+# NOTE: This script has only been tested on Ubuntu 22.04 LTS
+#!/bin/bash
+set -euo pipefail
+
+echo "--- Setting noninteractive and updating repos ---"
+export DEBIAN_FRONTEND=noninteractive
+echo "\$nrconf{restart} = 'l';" > /etc/needrestart/conf.d/90-autorestart.conf
+apt-get update
+
+# Install Docker and Compose
+echo "--- Installing Docker and ContainerLab ---"
+curl -sL https://containerlab.dev/setup | sudo bash -s "all"
+
+# Start docker
+systemctl start docker
---
+++ README.md
@@ -0,0 +1,12 @@
+# Installation
+
+## Docker and ContainerLab
+
+```
+./1_install_docker_and_clab.sh
+```
+
+## Slurpit
+
+- Follow the instructions here: https://gitlab.com/slurpit.io/images
+
